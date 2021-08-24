b0a6692b2833b67b6273e40841ab94a6974481c0
Gabriel programmer <83989808+Gabriel-FerreirasSantos@users.noreply.github.com>
2021-08-24 10:18:41 -0300

    update; part 2

--- Treinos/aula-002/treino-002.c
+++ Treinos/aula-002/treino-002.c
@@ -3,37 +3,34 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//função principal do programa
 void main(){
 	
 	//Permite usar acentos
 	setlocale(LC_ALL,"");
 	
-	//Imprime Olá
-	printf("Olá \n");
+	//Definindo variaveis
+	int a = 6, b = 3;
 	
-	//treino int
-	int a1 = 10;	
-	int a2 = 5;
-	printf("O valor de a é = %d \n", a1);
-	scanf("%d", &a1);	
-	printf("O valor de %d mudou para %d \n", a1, a2);
-		
+	//adição
+	printf("\n A adição de %d e %d é igual a = %d",a , b, a + b);
 	
-	//treino float
-	float b1 = 6.1;
-	float b2 = 5.9;
-	printf("O valor de colocado é = %f \n", b1);
-	scanf("%f", &b1);	
-	printf("O valor de anterior mudou para %f \n", b1 );
-
-
-	//treino char
-	char c1 = 'pneu';
-	char c2 = 'testando';
-	printf("O valor colocado é = %c \n", c1);
-	fflush(stdin);
-	scanf("%c", &c1);	
-	printf("O valor de %c mudou para %c \n",c1 , c2);
+	//Subtração
+	printf("\n A Subtração de %d e %d é igual a = %d",a , b, a - b);
 	
+	//divisão
+	printf("\n A divisão de %d e %d é igual a = %d",a , b, a / b);
+	
+	//Multiplicação
+	printf("\n A Multiplicação de %d e %d é igual a = %d",a , b, a * b);
+	
+	//Valor absoluto
+	printf("\n O valor absoluto de -3 = %d", abs(-3));
+	
+	//Resto da divisão
+	printf("\n o resto da divisão entre %d e %d é igual a = %d",a , b, a % b);
+	
+	//Pausa o programa apos a execução
+	system("pause");
 }
 
